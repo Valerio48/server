b76924edbd61e80ad62d26410a3f856e3c5b15ab
unknown <v.alunno@dstech.it>
2025-02-25 13:37:08 +0100

    first commit

---
+++ infrastruttura/src/test/java/it/testfinale/infrastruttura/InfrastrutturaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.testfinale.infrastruttura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrastrutturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
